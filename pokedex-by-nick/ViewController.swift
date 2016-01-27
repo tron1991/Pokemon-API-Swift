126fd8f237e9e0313ea2d9cec8fe5cff592dd853
Nicholas Ivanecky <ivanecky844@hotmail.com>
2016-01-26 20:54:07 -0500

    Initial Commit

---
+++ pokedex-by-nick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-nick
+//
+//  Created by Nick on 2016-01-26.
+//  Copyright © 2016 Nicholas Ivanecky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
